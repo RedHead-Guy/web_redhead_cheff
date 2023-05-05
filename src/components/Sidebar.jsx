796db705ad8bb19f2f766ded5bd275ff44eac2b1
RedHead-Guy <diyanovvv92@gmail.com>
2023-05-05 17:02:23 +0700

    fix bugs

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,12 +4,16 @@ import {BsPlus, BsFillLightbulbFill, BsGearFill, BsInstagram } from 'react-icons
 import {FaFire, FaPoo, FaInstagram} from 'react-icons/fa'
 import{TbBrandCakephp} from 'react-icons/tb'
 import SidebarIcon from './SidebarIcon'
+import Link from 'next/link'
 
 
 const Sidebar = () => {
   return (
    <div className='sidebar'>
-   <SidebarIcon icon ={<GiPieSlice size={50}/>} text="Десерты" />
+    <Link href='/#cakes'>
+      <SidebarIcon icon ={<GiPieSlice size={50}/>} text="Десерты" />
+    </Link>
+ 
    <SidebarIcon icon ={<GiBottledBolt  size={50}/>} text="Напитки" />
    <SidebarIcon icon ={<TbBrandCakephp size={50}/>} text="Основное" />
    <SidebarIcon icon ={<BsPlus size={50}/>} text="Ещё" />
